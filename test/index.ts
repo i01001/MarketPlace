dce5a1a036ba58f14a3db20cbd12a3500e3951d1
Ikhlas <ikhlaskhan007@gmail.com>
2022-05-09 02:16:55 +0400

    updated tests

--- test/index.ts
+++ test/index.ts
@@ -46,10 +46,58 @@ describe("Testing the Market Place Contract", () =>{
     });
 
     describe("Checking setNFT721ContractAddress is run correctly", () => {
-        it("Checks thesetNFT721ContractAddress is updated correctly or not", async () => {
+        it("Checks the setNFT721ContractAddress is updated correctly or not", async () => {
           await mP.setNFT721ContractAddress(mNFT721.address);
             expect(await mP.NFT721Contract()).to.be.equal(await mNFT721.address);
         })
+
+        it("Checks the setNFT1155ContractAddress is updated correctly or not", async () => {
+          await mP.setNFT1155ContractAddress(mNFT1155.address);
+            expect(await mP.NFT1155Contract()).to.be.equal(await mNFT1155.address);
+        })
+
+        it("Checks the setAuctionListingPrice is updated correctly or not", async () => {
+          await mP.setAuctionListingPrice(10**10);
+          let stringAuction = await (await mP.AuctionListingPrice()).toString();
+            await expect(stringAuction).to.be.equal(await (10**10).toString());
+        })
+
+        it("Checks the setListingPrice is updated correctly or not", async () => {
+          await mP.setListingPrice(10**10);
+          let stringAuction = await (await mP.ListingPrice()).toString();
+            await expect(stringAuction).to.be.equal(await (10**10).toString());
+        })
+
+        it("Checks the create Function is working correctly or not by creating NFT721", async () => {
+          await mP.connect(owner).createItem(false, "randomTOKENURIdata.json", 20);
+          expect(await mNFT721.connect(owner).balanceOf(owner.address)).to.be.equal(1);
+        })
+      
+        it("Checks the create Function is working correctly or not by creating NFT1155", async () => {
+          await mP.connect(owner).createItem(true, "randomTOKENURIdata.json", 20);
+          expect(await mNFT1155.connect(owner).balanceOf(owner.address, 1)).to.be.equal(20);
+        })
+
+        it("Checks the approve Function is working correctly or not for NFT721", async () => {
+          await mNFT721.connect(owner).setApprovalForAll(mP.address, true);
+          expect(await mNFT721.connect(owner).isApprovedForAll(owner.address, mP.address)).to.be.equal(true);
+        })
+
+        it("Checks the approve Function is working correctly or not for NFT1155", async () => {
+          await mNFT1155.connect(owner).setApprovalForAll(mP.address, true);
+          expect(await mNFT1155.connect(owner).isApprovedForAll(owner.address, mP.address)).to.be.equal(true);
+        })
+
+        it("Checks the List Function is working correctly or not for NFT721 created", async () => {
+          await mP.connect(owner).listItem(mNFT721.address, 1, false, 20, [], false, 10**10, { value: ethers.utils.parseEther("0.00000001") });
+          expect(await mNFT721.connect(owner).balanceOf(mP.address)).to.be.equal(1);
+        })
+
+        it("Checks the Buy Function is working correctly or not for NFT721 listed", async () => {
+          await mP.connect(signertwo).buyItem(1, { value: ethers.utils.parseEther("0.00000001") });
+          expect(await mNFT721.connect(owner).balanceOf(signertwo.address)).to.be.equal(1);
+        })
+
     })
 
   })
